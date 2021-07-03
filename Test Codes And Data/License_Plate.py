76c547aff6b276135ed7fdaf1d40ce66ef84cbaf
Popdev225 <popdev225@gmail.com>
2021-07-04 01:04:06 +0500

    Update License_Plate.py

--- Test Codes And Data/License_Plate.py
+++ Test Codes And Data/License_Plate.py
@@ -2,7 +2,7 @@
 import pytesseract
 import cv2
 
-img = cv2.imread('C:/Users/David Snake/Documents/6th Semester Documents/2.) DIGITAL IMAGE PROCESSING (THEORY & LAB)/LAB/Lab 2 images/Test Model 2.jpg')
+img = cv2.imread('/Test Model 2.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 thresh, binary = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY)
 
