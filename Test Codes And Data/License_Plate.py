b9a8888bf60edc17eab32d3d5a0baae579f99900
Popdev225 <popdev225@gmail.com>
2023-05-30 11:30:48 +0500

    Update License_Plate.py
    The path to tesseract-ocr was not specified in the previous commit, which would lead the program to not work as intended.

--- Test Codes And Data/License_Plate.py
+++ Test Codes And Data/License_Plate.py
@@ -2,6 +2,15 @@
 import pytesseract
 import cv2
 
+# Before starting, please note that you have to specify the path to tesseract-ocr
+# in order for pytesseract to work correctly. Path to tesseract-ocr is specified
+# as: pytesseract.pytesseract.tesseract_cmd = r'\Tesseract-OCR\tesseract.exe'
+# To know the path to the tesseract.exe, note down the path when installing
+# tesseract-ocr from https://github.com/UB-Mannheim/tesseract/wiki
+# Specify that path in "pytesseract.pytesseract.tesseract_cmd"
+
+pytesseract.pytesseract.tesseract_cmd = r'..\Tesseract-OCR\tesseract.exe'
+
 img = cv2.imread('/Test Model 2.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 thresh, binary = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY)
