908f5e3741ec3bdc205bda5c194029ea6cd72b59
Popdev225 <popdev225@gmail.com>
2023-05-30 11:29:59 +0500

    Update Detect_Handwrite_2.py
    The path to tesseract-ocr was not specified in the previous commit, which would lead the program to not work as intended.

--- Test Codes And Data/Detect_Handwrite_2.py
+++ Test Codes And Data/Detect_Handwrite_2.py
@@ -2,6 +2,15 @@
 import pytesseract
 import cv2
 
+# Before starting, please note that you have to specify the path to tesseract-ocr
+# in order for pytesseract to work correctly. Path to tesseract-ocr is specified
+# as: pytesseract.pytesseract.tesseract_cmd = r'\Tesseract-OCR\tesseract.exe'
+# To know the path to the tesseract.exe, note down the path when installing
+# tesseract-ocr from https://github.com/UB-Mannheim/tesseract/wiki
+# Specify that path in "pytesseract.pytesseract.tesseract_cmd"
+
+pytesseract.pytesseract.tesseract_cmd = r'..\Tesseract-OCR\tesseract.exe'
+
 img = cv2.imread('/HandwritingTest2.jpg')
 img = cv2.medianBlur(img, 5)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
